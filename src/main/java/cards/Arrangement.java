7aaec6f775cbae361910763d3f98771151714672
Matthew Callis <mfcallis@ncsu.edu>
2025-01-05 21:37:04 -0500

    Add card-to-kabbalistic map to arrangements

--- src/main/java/cards/Arrangement.java
+++ src/main/java/cards/Arrangement.java
@@ -1,10 +1,13 @@
 package cards;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.*;
+import tree.Element;
 @Entity
 public class Arrangement {
 	@Id
@@ -18,6 +21,16 @@ public class Arrangement {
 	@OneToMany
 	@JoinColumn(name = "deck_id", referencedColumnName = "deckId", insertable = false, updatable = false)
     private List<Card> cards;
+	
+	// Maps card values to qabbalistic elements
+	@OneToMany
+    @JoinTable(
+        name = "arrangement_elements",
+        joinColumns = @JoinColumn(name = "arrangement_id"),
+        inverseJoinColumns = @JoinColumn(name = "element_id")
+    )
+    @MapKeyColumn(name = "key") // The column to store the integer key
+    private Map<Integer, Element> elements = new HashMap<>();
 	
     // Constructor
     public Arrangement() {
